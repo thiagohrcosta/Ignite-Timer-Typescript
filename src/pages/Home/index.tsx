79941ee6f2bbe0a2cd18ed36c52c748f59c3df03
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-07-17 12:12:47 -0300

    Reset form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,15 +25,17 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
-      minutesAmount: 5,
+      minutesAmount: 0,
     },
   })
 
-  function handleCreateNewCycle(data: NewCycleFormData) {}
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
