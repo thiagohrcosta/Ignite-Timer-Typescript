dffc58a37746c0874e233b4df22f06b6bcd34331
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-07-25 22:12:02 -0300

    Adding code to stop cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -84,7 +84,7 @@ export const Separator = styled.div`
   align-items: center;
 `
 
-export const StartCountDownButton = styled.button`
+export const BaseCountDownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -96,18 +96,28 @@ export const StartCountDownButton = styled.button`
 
   gap: 0.5rem;
   font-weight: bold;
+  color: ${(props) => props.theme['gray-100']};
 
   cursor: pointer;
-
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountDownButton = styled(BaseCountDownButton)`
+  background: ${(props) => props.theme['green-500']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountDownButton = styled(BaseCountDownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
