144f15ebf46b02c709f7a2ee182bf95f9aec34c7
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-27 20:46:22 -0300

    Adding route context

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,39 +1,30 @@
+import { useContext } from 'react'
+import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
 export default function History() {
+  const { cycles } = useContext(CyclesContext)
+
   return (
     <HistoryContainer>
       <h1>Meu histórico</h1>
+
+      <pre>{JSON.stringify(cycles, null, 2)}</pre>
+
       <HistoryList>
         <table>
           <thead>
             <tr>
               <th>Tarefa</th>
               <th>Duração</th>
-              <th>Início</th>
+              <th>Duração</th>
               <th>Status</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>Tarefa</td>
-              <td>20min</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="red">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20min</td>
-              <td>Há 2 meses</td>
-              <td>
-                <Status statusColor="yellow">Concluído</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20min</td>
+              <td>20 minutos</td>
               <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
@@ -41,7 +32,7 @@ export default function History() {
             </tr>
             <tr>
               <td>Tarefa</td>
-              <td>20min</td>
+              <td>20 minutos</td>
               <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
@@ -49,7 +40,7 @@ export default function History() {
             </tr>
             <tr>
               <td>Tarefa</td>
-              <td>20min</td>
+              <td>20 minutos</td>
               <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
@@ -57,7 +48,7 @@ export default function History() {
             </tr>
             <tr>
               <td>Tarefa</td>
-              <td>20min</td>
+              <td>20 minutos</td>
               <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
@@ -65,10 +56,26 @@ export default function History() {
             </tr>
             <tr>
               <td>Tarefa</td>
-              <td>20min</td>
+              <td>20 minutos</td>
               <td>Há 2 meses</td>
               <td>
                 <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
               </td>
             </tr>
           </tbody>
