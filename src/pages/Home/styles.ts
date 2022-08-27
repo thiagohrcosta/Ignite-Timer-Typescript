47e3a414abad54d3ab937e0f8e168334f49aeeca
thiagohrcosta <thiagohrcosta86@gmail.com>
2022-08-27 19:49:36 -0300

    add context to form

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,7 +6,6 @@ export const HomeContainer = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   form {
     display: flex;
     flex-direction: column;
@@ -15,39 +14,34 @@ export const HomeContainer = styled.main`
   }
 `
 
-export const BaseCountDownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
   border-radius: 8px;
-
   display: flex;
   align-items: center;
   justify-content: center;
-
+  color: ${(props) => props.theme['gray-100']};
   gap: 0.5rem;
   font-weight: bold;
-  color: ${(props) => props.theme['gray-100']};
-
   cursor: pointer;
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
 `
 
-export const StartCountDownButton = styled(BaseCountDownButton)`
+export const StartCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['green-500']};
-
+  color: ${(props) => props.theme['gray-100']};
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
   }
 `
 
-export const StopCountDownButton = styled(BaseCountDownButton)`
+export const StopCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['red-500']};
-
   &:not(:disabled):hover {
     background: ${(props) => props.theme['red-700']};
   }
